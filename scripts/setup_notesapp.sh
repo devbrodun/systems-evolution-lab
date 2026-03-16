3ead890446b551e69260bff0ff27299b906904a4
Ekerin Oluwatimileyin <mosesekerin@gmail.com>
2026-03-16 21:20:44 +0100

    corrected potential redundancy in the script

--- scripts/setup_notesapp.sh
+++ scripts/setup_notesapp.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Cloning application repository..."
-sudo git clone https://github.com/mosesekerin/cloud-system-evolution.git /opt/notesapp
-sudo chown -R notesapp:notesapp /opt/notesapp
-
 echo "Installing application dependencies..."
 cd /opt/notesapp
 sudo -u notesapp npm install --omit=dev
